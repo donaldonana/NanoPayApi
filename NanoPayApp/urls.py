c259f8b01bfd5fa62bed4feae6948152b7950341
onana <onanadonald@gmail.com>
2022-04-05 18:07:27 +0100

    aucun message

--- NanoPayApp/urls.py
+++ NanoPayApp/urls.py
@@ -7,9 +7,9 @@ from NanoPayApp import views
 
 router = DefaultRouter()
 router.register('UserProfile', views.UserProfileViewSet)
-router.register('Compte', views.CompteViewSet)
-router.register('Transaction', views.TransactionViewSet)
-router.register('ParametreCarte', views.ParametreCarteViewSet)
+# router.register('Compte', views.CompteViewSet)
+# router.register('Transaction', views.TransactionViewSet)
+# router.register('ParametreCarte', views.ParametreCarteViewSet)
 
 
 
@@ -17,7 +17,7 @@ urlpatterns = [
 
     path('', include(router.urls)),
     path("login/", views.CustomAuthToken.as_view()),
-    path("change-password/", views.ChangePasswordView.as_view()),
+    # path("change-password/", views.ChangePasswordView.as_view()),
     path("logout/", views.Logout.as_view()),
     #path('user/{tlephone}/', views.PurchaseList.as_view()),
     url(r'^user/(?P<telephone>\d+)/$', views.UserList.as_view()),
