8ebbee965344658db812ebd7156098ced3b085ea
onana <onanadonald@gmail.com>
2022-07-19 08:38:35 +0100

    AppsComptes

---
+++ AppsComptes/urls.py
@@ -0,0 +1,28 @@
+from unicodedata import name
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+# from django.conf.urls import  url
+
+from AppsComptes import views
+from drf_yasg import openapi
+
+
+router = DefaultRouter() 
+
+urlpatterns = [ 
+
+    path('user/compte/<telephone>', views.UserComptesView.as_view(), name= 'AppsComptes'),
+    path('user/compte/', views.CompteCreateView.as_view(), name= 'UserCompte'),
+    path('user/compte/info/<numCompte>', views.RetrieveComptesView.as_view()),
+    path('user/compte/carte/toggle/', views.ToggleCompteView.as_view(), name= 'ToggleCarte'),
+    path('user/compte/carte/limite/', views.QuotidientLimiteView.as_view()),
+    path('user/compte/carte/plafond/', views.PaimentQuotidientView.as_view()),
+    path('carte/init/', views.CarteInitCreateAPIView.as_view()),
+    
+
+
+
+     
+	
+
+]
